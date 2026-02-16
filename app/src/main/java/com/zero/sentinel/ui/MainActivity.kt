c9677c2eb69c0cc81c12aa09944c9fcdb5495d84
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2026-02-17 00:11:57 +0700

    Fix compilation error: Add missing Log import in MainActivity

--- app/src/main/java/com/zero/sentinel/ui/MainActivity.kt
+++ app/src/main/java/com/zero/sentinel/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.view.accessibility.AccessibilityManager
 import android.widget.Button
 import android.widget.EditText
